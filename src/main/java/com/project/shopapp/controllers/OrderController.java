b3251fe9a989afee46ba16a1760e4972a64ab080
creolew <phamquochuy3005@gmail.com>
2024-03-11 10:11:29 +0700

    refractor Order Detail

--- src/main/java/com/project/shopapp/controllers/OrderController.java
+++ src/main/java/com/project/shopapp/controllers/OrderController.java
@@ -1,59 +1,78 @@
 package com.project.shopapp.controllers;
 
-
-import com.project.shopapp.dtos.OrderDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.project.shopapp.dtos.CategoryDTO;
+import com.project.shopapp.dtos.*;
+import com.project.shopapp.models.Order;
+import com.project.shopapp.services.IOrderService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("${api.prefix}/orders")
+@RequiredArgsConstructor
 public class OrderController {
-
+    private final IOrderService orderService;
     @PostMapping("")
-    public ResponseEntity<?> createOrder(@RequestBody @Valid OrderDTO orderDTO,  BindingResult result){
-        try{
-            if (result.hasErrors()) {
-                List<String> errorMessages = result.getFieldErrors().stream()
+    public ResponseEntity<?> createOrder(
+            @Valid @RequestBody OrderDTO orderDTO,
+            BindingResult result
+    ) {
+        try {
+            if(result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
                         .map(FieldError::getDefaultMessage)
-                        .collect(Collectors.toList());
+                        .toList();
                 return ResponseEntity.badRequest().body(errorMessages);
             }
-            return ResponseEntity.ok("create order successfully");
-        }catch(Exception e){
+            Order orderResponse = orderService.createOrder(orderDTO);
+            return ResponseEntity.ok(orderResponse);
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
-    @GetMapping("/{user_id}")
-    public ResponseEntity<?> getOrders(@Valid @PathVariable("user_id") Long userId){
+    @GetMapping("/user/{user_id}") // Thêm biến đường dẫn "user_id"
+    //GET http://localhost:8088/api/v1/orders/user/4
+    public ResponseEntity<?> getOrders(@Valid @PathVariable("user_id") Long userId) {
         try {
-            return ResponseEntity.ok("lay ra danh sach order tu user_id");
-        }catch (Exception e){
+            List<Order> orders = orderService.findByUserId(userId);
+            return ResponseEntity.ok(orders);
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
+    //GET http://localhost:8088/api/v1/orders/2
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrder(@Valid @PathVariable("id") Long orderId) {
+        try {
+            Order existingOrder = orderService.getOrder(orderId);
+            return ResponseEntity.ok(existingOrder);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
     @PutMapping("/{id}")
+    //PUT http://localhost:8088/api/v1/orders/2
+    //công việc của admin
     public ResponseEntity<?> updateOrder(
-        @Valid @PathVariable long id,
-        @Valid @RequestBody OrderDTO orderDTO) {
-        return ResponseEntity.ok("Cap nhat thong tin 1 order");
+            @Valid @PathVariable long id,
+            @Valid @RequestBody OrderDTO orderDTO) {
+
+        try {
+            Order order = orderService.updateOrder(id, orderDTO);
+            return ResponseEntity.ok(order);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteOrder(@Valid @PathVariable long id) {
-        return ResponseEntity.ok("Order deleted succesfully");
+    public ResponseEntity<String> deleteOrder(@Valid @PathVariable Long id) {
+        //xóa mềm => cập nhật trường active = false
+        orderService.deleteOrder(id);
+        return ResponseEntity.ok("Order deleted successfully.");
     }
-
-
 }
